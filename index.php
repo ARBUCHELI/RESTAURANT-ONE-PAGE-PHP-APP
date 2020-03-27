4ee5582d434ce34a6821d48a197a7d143532a1f9
rothwulf <radio_seattle@hotmail.com>
2020-03-27 17:30:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
 	
-	define("TITLE", "Home | Franklin's Fine Dining");
+	define("TITLE", "Home | Franklin's Fine Dining"); //Title of the Website.
 
-	include('includes/header.php');
+	include('includes/header.php'); //Code used to include the header.
 ?>
-
+	<!--This is the html section-->
 	<div id="philosophy">
-		<hr style="border: none; height: 18px; width: 114px; background: url('assets/img/hr.png') center center no-repeat; margin: 20px auto;">
+		<hr style="border: none; height: 18px; width: 114px; background: url('assets/img/hr.png') center center no-repeat; margin: 20px auto;"> <!--Decorative detail-->
 
-		<h1>Franklin's Philosophy of Fine Dining</h1>
-		<p>Here at Franklin's, we know that good food ins't just about how expensive the dish is.  We're not pompous, we're proud.  We're proud of our work, our quality, our environment, and our love for food and family.</p>
+		<h1>Franklin's Philosophy of Fine Dining</h1> <!--Title of the home content-->
+		<p>Here at Franklin's, we know that good food ins't just about how expensive the dish is.  We're not pompous, we're proud.  We're proud of our work, our quality, our environment, and our love for food and family.</p> <!--Welcome message-->
 
-		<p>Oh, and you can call us Frankie's.  You're family here.</p>
-		<hr style="border: none; height: 18px; width: 114px; background: url('assets/img/hr.png') center center no-repeat; margin: 20px auto;">
+		<p>Oh, and you can call us Frankie's.  You're family here.</p> <!--Welcome message-->
+		<hr style="border: none; height: 18px; width: 114px; background: url('assets/img/hr.png') center center no-repeat; margin: 20px auto;"> <!--Decorative detail-->
 		
 	</div><!-- philosophy -->
 
 <?php
-	include('includes/footer.php');
+	include('includes/footer.php'); //Code used to include the header.
 ?>
