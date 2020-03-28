3fbae0699e180a478480d1473c9bac0c3cdc336b
rothwulf <radio_seattle@hotmail.com>
2020-03-28 15:17:13 +0300

    Update dish.php

--- dish.php
+++ dish.php
@@ -1,8 +1,8 @@
 <?php 
 
-	define("TITLE", "Menu Item | Franklin's Fine Dining");
+	define("TITLE", "Menu Item | Franklin's Fine Dining"); //Title of the Website.//
 
-	include('includes/header.php');
+	include('includes/header.php'); //Code used to include de header.//
 
 
 
@@ -22,30 +22,30 @@
 
 	function suggestedTip($price, $tip) {
 
-		$totalTip = $price * $tip;
+		$totalTip = $price * $tip; //This function calculates the tip//
 		echo $totalTip;
 
 	}
 
 ?>
 
-<hr style="border: none; height: 18px; width: 114px; background: url('assets/img/hr.png') center center no-repeat; margin: 20px auto;">
+<hr style="border: none; height: 18px; width: 114px; background: url('assets/img/hr.png') center center no-repeat; margin: 20px auto;"> <!--Decorative detail-->
 
-<div id="dish">
+<div id="dish"> <!--html that includes the dishes, pulling the information from arrays.php file-->
 
 	<h1><?php echo $dish['title']; ?> <span class="price"><sup>$</sup><?php echo $dish['price']; ?></span></h1>
 	<p><?php echo $dish['blurb']; ?></p>
 
 	<br>
 
-	<p><strong>Suggested beverage: <?php echo $dish['drink']; ?></strong></p>
+	<p><strong>Suggested beverage: <?php echo $dish['drink']; ?></strong></p> <!--This information pulled from arrays.php file too-->
 	<p><em>Suggested Tip: <sup>$</sup><?php suggestedTip($dish['price'], 0.20); ?></em></p>
 
 </div><!-- dish -->
 
-<hr style="border: none; height: 18px; width: 114px; background: url('assets/img/hr.png') center center no-repeat; margin: 20px auto;">
+<hr style="border: none; height: 18px; width: 114px; background: url('assets/img/hr.png') center center no-repeat; margin: 20px auto;"> <!--Decorative detail-->
 
-<a href="menu.php" class="button previous">&laquo; Back to Menu</a>
+<a href="menu.php" class="button previous">&laquo; Back to Menu</a> <!--Button that sends back to menu-->
 
-<?php include('includes/footer.php'); ?>
+<?php include('includes/footer.php'); ?> //Code used to include the footer.
 
